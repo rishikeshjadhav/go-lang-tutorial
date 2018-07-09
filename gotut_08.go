9d0747e201a11262e2a9a70e2b3f762c83c20d78
Rishikesh Jadhav <rishijadhav2792@gmail.com>
2018-07-09 17:14:18 +0530

    Wait groups Basic

--- gotut_08.go
+++ gotut_08.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
 
 func say(s string) {
 	for i := 0; i < 3; i++ {
 		fmt.Println(s)
 		time.Sleep(time.Millisecond * 100)
 	}
+	wg.Done()
 }
 
 func main() {
 	fmt.Println("Go Routines")
-	say("Hey")
+	wg.Add(1)
+	go say("Hey")
+	wg.Add(1)
 	go say("There")
-
+	// wg.Wait()
+	wg.Add(1)
+	go say("Hi")
+	wg.Wait()
 	time.Sleep(time.Second)
 }
